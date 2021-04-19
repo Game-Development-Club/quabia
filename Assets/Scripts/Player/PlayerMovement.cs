3c5cb02b9d08e8b1a5819a24a207224632bd408e
Esteban Garcia <estebangarcia2121@gmail.com>
2021-04-19 10:00:42 -0700

    Added .idea to gitignore

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int walkingSpeed;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+    }
+}
